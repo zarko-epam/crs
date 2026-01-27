a5006a3e49e93b7e5316ce99aa4330c4c7312ea6
Zarko Celebic <zarko_celebic@epam.com>
2026-01-27 10:10:15 +0100

    Inital commit

---
+++ src/main/java/com/xm/crs/CrsApplication.java
@@ -0,0 +1,13 @@
+package com.xm.crs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrsApplication.class, args);
+	}
+
+}
